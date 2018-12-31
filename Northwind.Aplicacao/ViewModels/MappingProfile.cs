580b233a9bc62b3a9e3b0f05037131e152f88d7d
VitorNeilAvelino <avelino.vitor@gmail.com>
2018-12-31 12:46:21 -0200

    Remoção do mapeamento

--- Northwind.Aplicacao/ViewModels/MappingProfile.cs
+++ Northwind.Aplicacao/ViewModels/MappingProfile.cs
@@ -1,17 +1,12 @@
 ﻿using AutoMapper;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Northwind.Aplicacao.ViewModels
 {
- public class MappingProfile : Profile
+    public class MappingProfile : Profile
     {
         public MappingProfile()
         {
-            //CreateMap<ProductReadModel, ProductViewModel>();
+
         }
     }
 }
